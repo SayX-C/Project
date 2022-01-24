f32b8f863e15b5dafd1201e063a5fd4b952b0665
Afzal Sayyed <sayyed0786afzal@gmail.com>
2022-01-24 17:31:39 +0530

    called images & quotes from an Api

---
+++ project/api.php
@@ -0,0 +1,62 @@
+<?php
+class Api
+{
+    // private $apiKey = "OT18kUoMTgi3Kxa1eTaO1rQrkXDfu2djx9xfJbYy";
+    // private $apiUrl = "https://api.nasa.gov/planetary/apod?api_key=";
+    private $apiKey = "/900x900/?random/";
+    private $apiUrl = "https://source.unsplash.com/";
+    private $api;
+
+    private $quoteApiUrl = "https://quotes15.p.rapidapi.com/quotes/random/?language_code=en";
+    private $quoteApiKey = "396322c866mshdb64f84aca71b1dp1ff18bjsndd2a8a8b0c5d";
+    private $quoteApi;
+
+
+    private $headers = array();
+
+
+    public function __construct()
+    {
+
+        $this->api = $this->apiUrl . $this->apiKey;
+        $this->quoteApi = $this->quoteApiUrl;
+
+        $this->headers[] = "X-RapidAPI-Host: quotes15.p.rapidapi.com";
+        $this->headers[] = 'X-RapidAPI-Key: ' . $this->quoteApiKey;
+    }
+    public function getApiImage()
+    {
+        /* $json_data = file_get_contents($this->api);
+        $responseData = json_decode($json_data);
+        $imgUrl = $responseData->url; 
+        return $imgurl*/
+        // return $data;
+        return $this->api;
+    }
+    public function getApiQuote()
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $this->quoteApi,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => $this->headers,
+        ]);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $responseData = json_decode($response);
+            $quote = $responseData->content;
+            $obj = $responseData->originator;
+            $name = $obj->name;
+
+            return [
+                $quote,
+                $name
+            ];
+        }
+    }
+}
---
+++ project/index.php
@@ -0,0 +1,63 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require 'api.php';
+
+
+$api = new Api;
+
+$quoteApi = $api->getApiQuote();
+$imgUrl = $api->getApiImage();
+// var_dump($imgUrl);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css" type="text/css">
+
+    <title>Image</title>
+
+    <style>
+        body {
+            background-image: url(<?php echo $imgUrl; ?>);
+            background-size: auto;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-color: rgba(25, 25, 255, 0.5);
+        }
+
+        .quotes {
+            color: white;
+            font-size: 30px;
+
+        }
+
+        .aurthor {
+            color: white;
+            font-size: 15px;
+            font-weight: bold;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+
+<body class="container-fluid h-100 w-100 g-0">
+    <div class="bg-clr ">
+        <h1 class=" text-center text-danger">Project</h1>
+        <div class=" ">
+            <h3 class="quotes text-center "><?php echo $quoteApi[0] ?></h3>
+            <p class="aurthor text-center ">-<?php echo $quoteApi[1] ?></p>
+        </div>
+    </div>
+
+    <!-- script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
